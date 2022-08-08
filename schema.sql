f47522f3599e671caf6af7c673f2daa950e3f5bc
BregornOriginal <93630700+BregornOriginal@users.noreply.github.com>
2022-08-08 19:57:29 -0300

    create table of vet_clinic

--- schema.sql
+++ schema.sql
@@ -1,5 +1,11 @@
 /* Database schema to keep the structure of entire database. */
-
-CREATE TABLE animals (
-    name varchar(100)
+CREATE TABLE IF NOT EXISTS public.vet_clinic (
+    id integer NOT NULL GENERATED ALWAYS AS IDENTITY (
+        INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1
+    ),
+    name character varying(100) COLLATE pg_catalog."default",
+    date_of_birth date,
+    escape_attemps integer,
+    neutered boolean,
+    weight_kg numeric
 );
