0f3e3ffe101ce6d6cc28c8d24fa4ace056822327
BregornOriginal <93630700+BregornOriginal@users.noreply.github.com>
2022-08-09 18:43:42 -0300

    add queries of exercise

--- queries.sql
+++ queries.sql
@@ -65,3 +65,50 @@ FROM
 WHERE
   weight_kg >= 10.4
   AND weight_kg <= 17.3;
+
+SELECT
+  COUNT(name)
+FROM
+  animals;
+
+SELECT
+  COUNT(name)
+FROM
+  animals
+WHERE
+  escape_attemps = 0;
+
+SELECT
+  AVG(weight_kg)
+FROM
+  animals;
+
+SELECT
+  neutered,
+  MAX(escape_attemps)
+FROM
+  animals
+GROUP BY
+  neutered;
+
+SELECT
+  species,
+  MAX(weight_kg),
+  MIN(weight_kg)
+FROM
+  animals
+GROUP BY
+  species;
+
+SELECT
+  species,
+  date_of_birth,
+  AVG(escape_attemps)
+FROM
+  animals
+WHERE
+  date_of_birth BETWEEN '1990-01-01'
+  AND '2000-12-31'
+GROUP BY
+  species,
+  date_of_birth;
