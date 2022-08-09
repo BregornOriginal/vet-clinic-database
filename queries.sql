8b1ef3d23d4678245e7e81ec58b9f64d9ab7295f
BregornOriginal <93630700+BregornOriginal@users.noreply.github.com>
2022-08-08 21:23:38 -0300

    queries to vet_clinic database

--- queries.sql
+++ queries.sql
@@ -1,3 +1,68 @@
 /*Queries that provide answers to the questions from all projects.*/
+SELECT
+  *
+from
+  animals
+WHERE
+  name = 'Luna';
 
-SELECT * from animals WHERE name = 'Luna';
+SELECT
+  *
+FROM
+  vet_clinic
+WHERE
+  name LIKE '%mon';
+
+SELECT
+  name
+FROM
+  vet_clinic
+WHERE
+  date_of_birth BETWEEN '2016-01-01'
+  and '2019-12-30';
+
+SELECT
+  name
+FROM
+  vet_clinic
+WHERE
+  neutered IN (true)
+  AND escape_attemps IN (3);
+
+SELECT
+  date_of_birth
+FROM
+  vet_clinic
+WHERE
+  name IN ('Agumon')
+  OR name IN ('Pikachu');
+
+SELECT
+  name,
+  escape_attemps
+FROM
+  vet_clinic
+WHERE
+  weight_kg > 10.5;
+
+SELECT
+  *
+FROM
+  vet_clinic
+WHERE
+  neutered IN ('true');
+
+SELECT
+  *
+FROM
+  vet_clinic
+WHERE
+  name != ('Gabumon');
+
+SELECT
+  *
+FROM
+  vet_clinic
+WHERE
+  weight_kg >= 10.4
+  AND weight_kg <= 17.3;
