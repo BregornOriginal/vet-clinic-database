cf757f3c572ce32f3e43ba670dc7aec35b01ca95
BregornOriginal <93630700+BregornOriginal@users.noreply.github.com>
2022-08-10 16:59:02 -0300

    update animals table on owners_id

--- data.sql
+++ data.sql
@@ -238,3 +238,45 @@ SET
   )
 WHERE
   name = ('Pikachu');
+
+UPDATE
+  animals
+SET
+  owner_id = (
+    SELECT
+      id
+    FROM
+      owners
+    WHERE
+      full_name = 'Bob'
+  )
+WHERE
+  name = 'Plantmon';
+
+UPDATE
+  animals
+SET
+  owner_id = (
+    SELECT
+      id
+    FROM
+      owners
+    WHERE
+      full_name = 'Melody Pond'
+  )
+WHERE
+  name IN ('Charmander', 'Squirtle', 'Blossom');
+
+UPDATE
+  animals
+SET
+  owner_id = (
+    SELECT
+      id
+    FROM
+      owners
+    WHERE
+      full_name = 'Dean Winchester'
+  )
+WHERE
+  name IN ('Angemon', 'Boarmon');
