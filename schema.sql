659bdf917959f264f33d46e964907809f460747e
BregornOriginal <93630700+BregornOriginal@users.noreply.github.com>
2022-08-11 13:59:13 -0300

    create vets, specializations and visits tables

--- schema.sql
+++ schema.sql
@@ -31,3 +31,30 @@ CREATE TABLE IF NOT EXISTS public.species (
     name character varying(100) COLLATE pg_catalog."default",
     CONSTRAINT species_pkey PRIMARY KEY (id)
 );
+
+CREATE TABLE IF NOT EXISTS public.vets (
+    id integer NOT NULL GENERATED ALWAYS AS IDENTITY (
+        INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1
+    ),
+    name character varying(50) COLLATE pg_catalog."default",
+    age integer,
+    date_of_graduation date,
+    CONSTRAINT vets_pkey PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS public.specializations (
+    specie_id integer NOT NULL,
+    vets_id integer NOT NULL,
+    CONSTRAINT specializations_pkey PRIMARY KEY (specie_id, vets_id),
+    CONSTRAINT specie_id FOREIGN KEY (specie_id) REFERENCES public.species (id) MATCH SIMPLE ON UPDATE NO ACTION ON DELETE NO ACTION,
+    CONSTRAINT vets_id FOREIGN KEY (vets_id) REFERENCES public.vets (id) MATCH SIMPLE ON UPDATE NO ACTION ON DELETE NO ACTION
+);
+
+CREATE TABLE IF NOT EXISTS public.visits (
+    animals_id integer NOT NULL,
+    vets_id integer NOT NULL,
+    visit_date date,
+    CONSTRAINT visits_pkey PRIMARY KEY (animals_id, vets_id),
+    CONSTRAINT animals_id FOREIGN KEY (animals_id) REFERENCES public.animals (id) MATCH SIMPLE ON UPDATE NO ACTION ON DELETE NO ACTION,
+    CONSTRAINT vets_id FOREIGN KEY (vets_id) REFERENCES public.vets (id) MATCH SIMPLE ON UPDATE NO ACTION ON DELETE NO ACTION
+);
